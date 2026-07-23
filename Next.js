6facdfb56d68c8b8bc2160a016fa67eba2b3da99
sasha Creates <neocitieshtmlcssjavascript@gmail.com>
2026-07-23 17:31:52 +0600

    Create Next.js

---
+++ Next.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Главная страница (Аналог index.html)</h1>
+      <p>Добро пожаловать в Next.js!</p>
+    </div>
+  );
+}
